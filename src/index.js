ddaa9732a4639b609f746b3ecc6c055e64f0ea08
Tushar Chaudhari <45455985+Tushar-Chaudhari-04@users.noreply.github.com>
2022-07-10 22:05:20 +0530

    JSX Basics

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDom from "react-dom";
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+const name = "Tushar Chaudhari";
+const year = new Date().getFullYear();
+ReactDom.render(
+  <div>
+    <p>Created by {name}</p>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById("root")
 );
