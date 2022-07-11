b7c0cb71cdc509a6f6b7f1d02fbebdfc124d2940
Tushar Chaudhari <45455985+Tushar-Chaudhari-04@users.noreply.github.com>
2022-07-11 10:51:30 +0530

    Html Basics in React

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from "react";
 import ReactDom from "react-dom";
+import "./styles.css";
 
 const name = "Tushar Chaudhari";
 const year = new Date().getFullYear();
 ReactDom.render(
   <div>
-    <p>Created by {name}</p>
+    <h1 className="heading">{name}</h1>
     <p>Copyright {year}</p>
+    <h2 style={{ color: "blue" }}>Indian Food</h2>
+    <img
+      src="https://image.shutterstock.com/image-photo/
+    indian-food-curry-butter-chicken-260nw-1304901667.jpg"
+      alt="indianfood"
+    />
+    {/* <div>
+      <br />
+    </div> */}
+    <img
+      src="https://encrypted-tbn0.gstatic.com/
+    images?q=tbn:ANd9GcR9BAolfVoRqgQwfILsrzJIUGQ4uPKrpcFVcg&usqp=CAU"
+      alt="food"
+    />
   </div>,
   document.getElementById("root")
 );
